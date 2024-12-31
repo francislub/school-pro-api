086d79010005243a31b3f14e78bacf6ba65a13a4
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2024-12-31 04:28:58 -0800

    morere

--- src/routes/school.ts
+++ src/routes/school.ts
@@ -4,7 +4,7 @@ const schoolRouter = express.Router();
 
 schoolRouter.post("/schools", createSchool);
 schoolRouter.get("/schools", getSchools);
-schoolRouter.get("/schools:id", getSchoolById);
+schoolRouter.get("/schools/:id", getSchoolById);
 // schoolRouter.get("/customers/:id", getCustomerById);
 // schoolRouter.get("/api/v2/customers", getV2Customers);
 
