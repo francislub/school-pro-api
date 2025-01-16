ecb8495647cd3a5d1b23f331110e984b7cdba798
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2025-01-16 07:16:29 -0800

    changes

--- src/routes/students.ts
+++ src/routes/students.ts
@@ -5,7 +5,7 @@ const studentRouter = express.Router();
 studentRouter.post("/students", createStudent);
 studentRouter.get("/students", getStudents);
 studentRouter.get("/students/school/:schoolId", getStudentsBySchoolId);
-studentRouter.get("/students/parent/:parentlId", getStudentsByParentId);
+studentRouter.get("/students/parent/:parentId", getStudentsByParentId);
 studentRouter.get("/students/seq/:schoolId", getNextStudentSquence);
 // studentRouter.get("/customers/:id", getCustomerById);
 // studentRouter.get("/api/v2/customers", getV2Customers);
