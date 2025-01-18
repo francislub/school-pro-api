583f2c6c7924c758b21c9f31a18c102914a7809e
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2025-01-18 00:32:53 -0800

    about fee

--- src/routes/school-fee.ts
+++ src/routes/school-fee.ts
@@ -1,10 +1,9 @@
-import { getPeriodsByYear } from "@/controllers/periods";
-import { createSchoolFee } from "@/controllers/school-fees";
+import { createSchoolFee, getSchoolFees } from "@/controllers/school-fees";
 import express from "express";
 const schoolFeeRouter = express.Router();
 
 schoolFeeRouter.post("/school-fees", createSchoolFee);
-schoolFeeRouter.get("/school-fees/:schoolId", getPeriodsByYear);
+schoolFeeRouter.get("/school-fees/:schoolId", getSchoolFees);
 // schoolFeeRouter.get("/periods/grouped/:schoolId", getSchoolFees);
 // periodRouter.get("/departments/school/:schoolId", getDepartmentsBySchoolId);
 // periodRouter.get("/departments/brief/:schoolId", getBriefDepartments);
